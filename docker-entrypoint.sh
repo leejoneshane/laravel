cee8dcc4e82e2e843ad4c41d5dc8cfb484ea0367
Sean Lee <leejoneshane@gmail.com>
2017-10-11 07:01:27 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 set -euo pipefail
 
-php artisan voyager:admin ${MAIL} --create
+if [[ "${MAIL}" != "your@mail.addr" ]]; then
+  php artisan voyager:admin ${MAIL} --create
+fi
+
+if [[ "${DB_HOST}" != "db" ]]; then
+  php artisan migrate:install
+fi
 
 rm -f /run/apache2/httpd.pid
 exec httpd -DFOREGROUND
