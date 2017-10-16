c47eceb9eb6cd3bad778fab0183482955645b586
Sean Lee <leejoneshane@gmail.com>
2017-10-15 20:26:30 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 set -euo pipefail
 
-if [[ "${MAIL}" != "your@mail.addr" ]]; then
-  php artisan voyager:admin ${MAIL} --create
+if [[ "${DB_HOST}" != "db" ]]; then
+  sed -ri \
+      -e 's!^(DB_HOST=).*$!\1 ${DB_HOST}!g' \
+      -e 's!^(DB_DATABASE=).*$!\1${DB_DATABASE}!g' \
+      -e 's!^(DB_USERNAME=).*$!\1${DB_USERNAME}!g' \
+      -e 's!^(DB_PASSWORD=).*$!\1${DB_PASSWORD}!g' \
+      /var/www/localhost/htdocs/.env
+  php artisan voyager:install
 fi
 
-if [[ "${DB_HOST}" != "db" ]]; then
-  php artisan migrate:install
-  php artisan voyager:install
+if [[ "${MAIL}" != "your@mail.addr" ]]; then
+  sed -ri -e "s/^(\s*ServerAdmin).*$/\1 ${MAIL}/g" /etc/apache2/httpd.conf
+  php artisan voyager:admin ${MAIL} --create
 fi
 
 rm -f /run/apache2/httpd.pid
