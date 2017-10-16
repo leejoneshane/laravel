c0c37fe3f6949432a9243329dfc16bbd6e6f05aa
Sean Lee <leejoneshane@gmail.com>
2017-10-15 20:44:10 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,12 +2,6 @@
 set -euo pipefail
 
 if [[ "${DB_HOST}" != "db" ]]; then
-  sed -ri \
-      -e 's!^(DB_HOST=).*$!\1 ${DB_HOST}!g' \
-      -e 's!^(DB_DATABASE=).*$!\1${DB_DATABASE}!g' \
-      -e 's!^(DB_USERNAME=).*$!\1${DB_USERNAME}!g' \
-      -e 's!^(DB_PASSWORD=).*$!\1${DB_PASSWORD}!g' \
-      /var/www/localhost/htdocs/.env
   php artisan voyager:install
 fi
 
