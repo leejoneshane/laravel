33eb335893bd72db2d5950405d1a021860e3205a
Sean Lee <leejoneshane@gmail.com>
2017-10-15 22:47:35 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ fi
 
 if [[ "${MAIL}" != "your@mail.addr" ]]; then
   sed -ri -e "s/^(\s*ServerAdmin).*$/\1 ${MAIL}/g" /etc/apache2/httpd.conf
-  php artisan voyager:admin ${MAIL} --create
+  echo -e "admin\n${WEB_PASSWORD}\n" | php artisan voyager:admin ${MAIL} --create
 fi
 
 rm -f /run/apache2/httpd.pid
