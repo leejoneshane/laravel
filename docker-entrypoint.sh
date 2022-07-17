2b0c92def0139d326aa83bae47937714949c83bd
leejoneshne <leejoneshane@gmail.com>
2022-07-17 19:32:14 +0800

    test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -euo pipefail
-  
-if [[ "$@" -eq "fetch" || ! -e /var/www/html/artisan ]]; then
+
+echo $1
+
+if [[ "${1}" -eq "fetch" || ! -e /var/www/html/artisan ]]; then
   cp -Rp /root/html/* /var/www/html
   cp -Rp /root/html/.[^.]* /var/www/html
   chown -R www-data:www-data /var/www
@@ -16,7 +18,7 @@ else
   php artisan telescope:install
 fi
 
-if [[ "$@" -eq "init" ]]; then
+if [[ "${1}" -eq "init" ]]; then
   echo -e "yes\nyes\nyes\n" | php artisan migrate:refresh
   echo -e "0" | php artisan vendor:publish
   php artisan -q make:auth
